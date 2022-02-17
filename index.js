3e5710274b7bb515c4084d358e8a931dbc6d9b7b
Loadren <sebagh_aaron@hotmail.com>
2022-02-17 02:22:32 +0100

    config.js iindex.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const Client = require("./structures/client.js");
-const config = require("./config.js");
+
+let config;
+
+try {
+	config = require("../config.js");
+} catch (e) {
+	console.log("Config file not found, using environment variables.")
+}
+
+config.bottoken = process.env.bottoken || config.bottoken;
+
 const client = new Client();
 
 client.init(config.bottoken);
