400c8474c80ff4421fa49a49438bf40e11eade1f
Loadren <sebagh_aaron@hotmail.com>
2022-02-17 02:04:23 +0100

    adding fake config

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+	//	These values will be ignored if you have set the environment variables (should be in uppercase)
+	
+		prefix: "",
+		bottoken: "",
+		geniusapitoken: "",
+		cookies:"",
+};
