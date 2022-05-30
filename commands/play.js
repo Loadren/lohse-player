bb00140a7046f32ebe873b4769e8eb40ef06afe4
Loadren <sebagh_aaron@hotmail.com>
2022-05-30 17:53:02 +0200

    ytdl-core

--- commands/play.js
+++ commands/play.js
@@ -1,75 +1,43 @@
 const Command = require("../structures/command.js");
 const { QueryType } = require('discord-player');
-const playdl = require("play-dl");
-
-let config = {};
-
-try {
-    config = require("../config.js");
-} catch (e) {
-}
-
-/*config.cookies = process.env.COOKIES || config.cookies;
-
-if (config.cookies)
-    playdl.setToken({
-        youtube: {
-            cookie: config.cookies
-        }
-    });*/
 
 module.exports = new Command({
-    name: "play",
+	name: "play",
     aliases: ['p'],
-    description: "Plays the song specified",
-    permission: "SEND_MESSAGES",
-    async run(message, args, client) {
-        if (!message.member.voice.channelId)
+	description: "Plays the song specified",
+	permission: "SEND_MESSAGES",
+	async run(message, args, client) {
+        if(!message.member.voice.channelId)
             return message.reply({ embeds: [{ description: `You are not in a voice channel!`, color: 0xb84e44 }] });
-        if (message.guild.me.voice.channelId && message.member.voice.channelId !== message.guild.me.voice.channelId)
+        if(message.guild.me.voice.channelId && message.member.voice.channelId !== message.guild.me.voice.channelId)
             return message.reply({ embeds: [{ description: `You are not in my voice channel!`, color: 0xb84e44 }] });
-        if (!args[1]) {
+        if(!args[1]) {
             const queue = client.player.getQueue(message.guild);
-            if (queue && queue.playing) { // resume
+            if(queue && queue.playing) { // resume
                 const paused = queue.setPaused(false);
-                if (paused) message.react('▶️');
+                if(paused) message.react('▶️');
             }
             return;
         }
-
-        if (!message.guild.me.permissionsIn(message.member.voice.channel).has(client.requiredVoicePermissions)) return;
+        
+        if(!message.guild.me.permissionsIn(message.member.voice.channel).has(client.requiredVoicePermissions)) return;
 
         let query = args.slice(1).join(" ");
         const searchResult = await client.player.search(query, { requestedBy: message.author, searchEngine: QueryType.AUTO })
         if (!searchResult || !searchResult.tracks.length)
             return message.channel.send({ embeds: [{ description: `No results found!`, color: 0xb84e44 }] });
 
+        
+        var queue = client.player.getQueue(message.guild); // Need to get the queue even if undefined to check if already in vocal. No need to recreate the queue
 
-        var queue = client.player.getQueue(message.guild);
-
-        if (!queue)
-            queue = await client.player.createQueue(message.guild, {
-                metadata: { channel: message.channel },
+        if(!queue)
+            queue = await client.player.createQueue(message.guild,{ metadata: { channel: message.channel },
 
                 bufferingTimeout: 1000,
                 disableVolume: false, // disabling volume controls can improve performance
                 leaveOnEnd: false,
                 leaveOnStop: true,
-                leaveOnEmpty: true,
-
-                async onBeforeCreateStream(track, source, _queue) {
-                    let vid;
-                    try {
-                        if (track.url.includes("youtube.com"))
-                            vid = (await playdl.stream(track.url, { discordPlayerCompatibility: true })).stream;
-                        else
-                            vid = (await playdl.stream(await playdl.search(`${track.author} ${track.title} lyric`, { limit: 1, source: { youtube: "video" } }).then(x => x[0].url), { discordPlayerCompatibility: true })).stream;
-                    } catch {
-                        queue.metadata.channel.send({ embeds: [{ description: `An error occurred while attempting to play [${track.title}](${track.url}).`, color: 0xb84e44 }] });
-                        vid = (await playdl.stream("https://www.youtube.com/watch?v=Wch3gJG2GJ4", { quality: 0 })).stream; // a 1 second video. if u have a better way to do this, feel free to open a PR/issue :)
-                    }
-                    return vid;
-                }
+                leaveOnEmpty: true
             });
         let justConnected;
         try {
@@ -81,8 +49,8 @@ module.exports = new Command({
             client.player.deleteQueue(message.guild);
             return message.channel.send({ embeds: [{ description: `Could not join your voice channel!`, color: 0xb84e44 }] });
         }
-        if (searchResult.playlist) searchResult.tracks[0].playlist = searchResult.playlist;
+        if(searchResult.playlist) searchResult.tracks[0].playlist = searchResult.playlist;
         await searchResult.playlist ? queue.addTracks(searchResult.tracks) : queue.addTrack(searchResult.tracks[0]);
-        if (justConnected || !queue.playing) queue.play();
-    }
+        if(justConnected || !queue.playing) queue.play(); // Here to check if already in vocal or just Connected.
+	}
 });
